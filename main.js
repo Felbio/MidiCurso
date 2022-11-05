57ac4ecf91899b60a91cce0ca84cf7e30da22a18
Felipe Oliveira <felipe.oliveira.ti@walljobs.com.br>
2022-11-05 00:40:16 -0300

    JavaScript with resource for

--- main.js
+++ main.js
@@ -2,21 +2,18 @@ function tocaSom(idElementAudio){
     document.querySelector(idElementAudio).play();
 }
 
-
 const listaTeclas = document.querySelectorAll('.tecla');
 
-let count = 0;
-while (count < listaTeclas.length){
+
+for (let count = 0; count < listaTeclas.length; count++){
     
     const tecla = listaTeclas[count];
     const instrumento = tecla.classList[1];
     //template string
     const idAudio = `#som_${instrumento}`;
-    // console.log(idAudio);
-
+    
     tecla.onclick = function(){
         tocaSom(idAudio);
     }
-    count++;
-    // console.log(count);
+    
 }
